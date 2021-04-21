459f23bbf60887eeba66f629a11a421d8398ee3a
Glowbase <45991010+glowbase@users.noreply.github.com>
2021-04-21 16:41:19 +1000

    FIX: CORS

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -2,40 +2,37 @@ const gc = require('../config');
 const path = require('path');
 const bucket = gc.bucket('pyro-uploads');
 
- function generateRandomName() {
-  return Math.random().toString(36).substr(2, 9);
+function genName() {
+	return Math.random().toString(36).substr(2, 9);
 }
 
- const uploadFile = (uid, fileName, type, limit) => new Promise((resolve, reject) => {
-  try {
-    let file
-    if (type == 'avatar') {
-      file = bucket.file(`avatars/${uid + path.parse(fileName).ext}`);
-    } else {
-      file = bucket.file(`userUploads/${uid}/${generateRandomName() + path.parse(fileName).ext}`);
-    }
+const uploadFile = (uid, fileName, type, limit) => new Promise(async (resolve, reject) => {
+	try {
+		const extension = path.parse(fileName).ext;
+		const location = type == 'avatar' ? `avatars/${uid}.gif` : `uploads/${uid}/${genName() + extension}`;
+		const file = bucket.file(location);
 
-    let oldDate = new Date()
-    let date = new Date(oldDate.getTime() + 5*60000);
- 
-    const options = {
-      expires: date.toISOString(),
-      conditions: [
-        ['content-length-range', 0, limit * 1024 * 1024],
-      ],
-      fields: {
-        acl: 'public-read'
-      }
-    };
+		// Set expiration date for authentication token
+		const oldDate = new Date();
+		const date = new Date(oldDate.getTime() + 5 * 60000); // 5 minute expiration
+	
+		const options = {
+			expires: date.toISOString(),
+			conditions: [
+				['content-length-range', 0, limit * 1024 * 1024], // Sets limit for file upload
+			],
+			fields: {
+				acl: 'public-read'
+			}
+		};
 
-    file.generateSignedPostPolicyV4(options).then(function(data) {
-      const response = data[0];
-      resolve(response)
-
-});
-  } catch (err) {
-    reject(err)
-  }
+		// Generate policy document
+		const data = await file.generateSignedPostPolicyV4(options);
+		
+		resolve(data[0]);
+  	} catch (error) {
+    	reject(error);
+  	}
 });
 
 module.exports = uploadFile;
--- index.js
+++ index.js
@@ -1,31 +1,38 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const uploadFile = require('./helpers/helpers.js');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
 app.post('/cdn', async (req, res) => {
-  try {
-    let ember = (req.body.ember == 'true');
-    const limit = ember ? 100 : 10
-    data = await uploadFile(req.body.uid, req.body.file, req.body.type, limit);
-    res.status(200).json({
-        success: true,
-        message: "File authed successfuly",
-        POST: data
-    });
-    } catch (error) {
-    res.status(418).json({
-        success: false,
-        error: error.message,
-        usage: "file = name of the file; uid = userid; type = either avatar or userUpload"
-      });
-    }
+	try {
+			const ember = req.body.ember == 'true';
+			const limit = ember ? 100 : 50;
+			const { uid, file, type } = req.body;
+
+			const data = await uploadFile(uid, file, type, limit);
+
+			res.status(200).json({
+				success: true,
+				message: "File authed successfuly",
+				POST: data
+			});
+		} catch (error) {
+			res.status(418).json({
+				success: false,
+				error: error.message,
+				usage: "file = name of the file; uid = userid; type = either avatar or userUpload"
+			});
+
+			console.error(error.message);
+		}
 });
 
 app.listen(8080, () => {
-  console.log('Uploader listning on port 8080')
+	console.log('Uploader listning on port 8080')
 });
